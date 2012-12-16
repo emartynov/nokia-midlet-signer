25677593c7c07a9283a58bc4709e8ea8d7981cde
U-Eugen-PC\Eugen <Eugen@Eugen-PC.(none)>
2012-12-16 10:28:46 +0100

    initial commit

---
+++ engine/src/main/java/com/ebuddy/nokia/s40/NokiaSigner.java
@@ -0,0 +1,12 @@
+package com.ebuddy.nokia.s40;
+
+/**
+ *
+ */
+public class NokiaSigner
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ engine/src/test/java/com/ebuddy/nokia/s40/NokiaSignerTest.java
@@ -0,0 +1,14 @@
+package com.ebuddy.nokia.s40;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple NokiaSigner.
+ */
+public class NokiaSignerTest {
+
+    @Test
+    public void testApp() {
+    }
+}
